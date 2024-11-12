9d623a73c4b3a410e2a793260468a55b2b15c4b0
oualidelh <elhouarioualid@gmail.com>
2024-11-12 23:13:00 +0100

    Initial commit

--- app/tiptap/editor.tsx
+++ app/tiptap/editor.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import NotificationsPopover from "../notifications-popover";
 import { useEditor, EditorContent, Editor } from "@tiptap/react";
 import { useLiveblocksExtension, FloatingComposer, FloatingThreads, AnchoredThreads } from "@liveblocks/react-tiptap";
 import StarterKit from "@tiptap/starter-kit";
 import { Toolbar } from "./Toolbar";
 import { useThreads } from "@liveblocks/react";
 import { useIsMobile } from "./use-is-mobile";
-import VersionsDialog from "../version-history-dialog";
 
 export default function TiptapEditor() {
   const liveblocks = useLiveblocksExtension();
@@ -29,11 +26,6 @@ export default function TiptapEditor() {
 
   return (
     <div className="relative min-h-screen flex flex-col">
-
-      <div className="h-[60px] flex items-center justify-end px-4 border-b border-border/80 bg-background">
-        <VersionsDialog editor={editor} />
-        <NotificationsPopover />
-      </div>
       <div className="relative flex flex-row justify-between w-full py-16 xl:pl-[250px] pl-[100px] gap-[50px]">
         <div className="relative flex flex-1 flex-col gap-2">
           <Toolbar editor={editor} />
